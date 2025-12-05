c1ee360a84fbedff7501ad0e722e6e90ed4dd1f2
Mathieu Langumier <mathieu.langumier@agixis.com>
2025-12-05 13:17:25 +0100

    Feat: Setting up project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from "./features/home/home";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component : Home, title: "Accueil" },
+  // { path: 'login', component : Login, title: "Connexion" },
+  // { path: 'register', component : Register, title: "Inscription" },
+  { path: "**", redirectTo: "", pathMatch: "full" }
+];
