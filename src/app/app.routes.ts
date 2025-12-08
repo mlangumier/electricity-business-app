6d9ccd6791b795a9f8098aaf9ba5f97beacec6b6
Mathieu Langumier <mathieu.langumier@agixis.com>
2025-12-08 18:57:29 +0100

    Feat: DDD structure  & base  components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
+import { Login } from "./core/auth/pages/login/login";
+import { Register } from "./core/auth/pages/register/register";
 import { Home } from "./features/home/home";
+import { Dashboard } from "./features/user/pages/dashboard/dashboard";
 
 export const routes: Routes = [
-  { path: '', component : Home, title: "Accueil" },
-  // { path: 'login', component : Login, title: "Connexion" },
-  // { path: 'register', component : Register, title: "Inscription" },
+  { path: '', component: Home, title: "Accueil | Electricity Business" },
+
+  { path: 'login', component: Login, title: "Connexion" },
+  { path: 'register', component: Register, title: "Inscription" },
+
+  { path: 'dashboard', component: Dashboard, title: "Dashboard" }, // Add guard
   { path: "**", redirectTo: "", pathMatch: "full" }
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,18 @@
-import { Component, signal } from '@angular/core';
+import { DATE_PIPE_DEFAULT_OPTIONS } from "@angular/common";
+import { Component, LOCALE_ID, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from "./core/layout/footer/footer";
+import { Header } from "./core/layout/header/header";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ RouterOutlet, Header, Footer ],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  providers: [ { provide: LOCALE_ID, useValue: "fr" }, {
+    provide: DATE_PIPE_DEFAULT_OPTIONS,
+    useValue: { dateFormat: "dd-MM-YYYY" }
+  } ]
 })
 export class App {
   protected readonly title = signal('Electricity Business');
