c10fe5d6f85438607a5049230ad2e829cb0d21ab
Mathieu Langumier <mathieu.langumier@agixis.com>
2025-12-19 18:02:32 +0100

    Feat(WIP): setting up layout

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,11 +1,13 @@
+import { NgOptimizedImage } from "@angular/common";
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [ NgOptimizedImage, RouterLink ],
   templateUrl: './header.html',
   styles: ``,
 })
 export class Header {
-
+  //Import array: navigationItemsPublic
 }
