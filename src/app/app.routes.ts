18af1b2df0615c12426ceb6f125c710f71c9d83f
Mathieu Langumier <mathieu.langumier@agixis.com>
2025-12-19 18:02:32 +0100

    Feat(WIP): setting up layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,14 +7,21 @@ import { Dashboard } from "./features/user/dashboard/dashboard";
 //TODO: transform routes to lazy-loaded routes with "loadComponent"
 
 export const routes: Routes = [
-  { path: '', component: Home, title: "Accueil | Electricity Business" },
+  {
+    path: '',
+    loadComponent: () => import("./features/public/home/home").then(m => m.Home),
+    title: "Electricity Business"
+  },
+  // { path: '', component: Home, title: "Electricity Business" },
 
   { path: 'login', component: Login, title: "Connexion" },
   { path: 'register', component: Register, title: "Inscription" },
 
   // { path: 'app', component: Dashboard, title: "Dashboard" }, // Add guard
-  { path: 'app', component: Dashboard, title: "Dashboard", children: [
+  {
+    path: 'app', component: Dashboard, title: "Mon Espace | Electricity Business", children: [
       // { path: 'profile', component: UserProfile, title: "Profil" }
-    ] }, // Add guard
+    ]
+  }, // Add guard
   { path: "**", redirectTo: "", pathMatch: "full" }
 ];
